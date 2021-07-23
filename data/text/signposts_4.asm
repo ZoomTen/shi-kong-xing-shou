5d18ed26b9ad4662992e338b4f84201edb834fb6
Zumi Daxuya <daxuya.zumi+code@protonmail.com>
2021-07-23 22:45:43 +0700

    idk

---
+++ data/text/signposts_4.asm
@@ -0,0 +1,99 @@
+
+text_0c_4216::
+	text_sign
+	text "神", 0
+	text "木", 1
+	text "村", 4
+	done
+
+text_0c_421e::
+	text_sign
+	text "←", 9
+	text "奇", 1
+	text "蹟"
+	text "之", 0
+	text "泉", 7
+	done
+
+text_0c_4229::
+	text_sign
+	text "↓", 9
+	text "雷", 0
+	text "達", 1
+	text "站", 5
+	done
+
+text_0c_4233::
+	text_sign
+	text "→", 9
+	text "神", 0
+	text "木", 1
+	text "村", 4
+	done
+
+text_0c_423d::
+	text_sign
+	text "↑", 9
+	text "神", 0
+	text "木", 1
+	text "村", 4
+	line
+	text "↓", 9
+	text "雷", 0
+	text "達", 1
+	text "站", 5
+	done
+
+text_0c_4250::
+	text_sign
+	text "雷", 0
+	text "達", 1
+	text "站", 5
+	done
+
+text_0c_4258::
+	text_sign
+	text "人", 0
+	text "魚", 1
+	text "村", 4
+	done
+
+text_0c_4260::
+	text_sign
+	text "海", 0
+	text "巫", 7
+	text "之", 0
+	text "殿", 7
+	done
+
+text_0c_426a::
+	text_sign
+	text "生", 1
+	text "之", 0
+	text "谷", 7
+	done
+
+text_0c_4272::
+	text_sign
+	text "↑", 9
+	text "生", 1
+	text "之", 0
+	text "谷", 7
+	line
+	text " ", 0
+	text "死之"
+	text "谷", 7
+	done
+
+text_0c_4283::
+	text_sign
+	text "↓", 9
+	text "神", 0
+	text "木", 1
+	text "村", 4
+	line
+	text "→", 9
+	text "地", 0
+	text "熱"
+	text "城", 4
+	done
---
+++ data/text/signposts_5.asm
@@ -0,0 +1,58 @@
+
+text_0c_42a5::
+	text_sign
+	text "星", 0
+	text "夢", 2
+	text "城", 4
+	done
+
+text_0c_42ad::
+	text_sign
+	text "往", 3
+	text "學", 1
+	text "校", 4
+	done
+
+text_0c_42b5::
+	text_sign
+	text "↓", 9
+	text "海", 0
+	text "邊", 4
+	done
+
+text_0c_42bd::
+	text_sign
+	text "←", 9
+	text "永", 6
+	text "凍", 0
+	text "園", 8
+	done
+
+text_0c_42c7::
+	text_sign
+	text "星", 0
+	text "夢", 2
+	text "城", 4
+	text "醫", 1
+	text "院", 8
+	done
+
+text_0c_42d3::
+	text_sign
+	text "永", 6
+	text "凍", 0
+	text "園", 8
+	done
+
+text_0c_42db::
+	text_sign
+	text "怪", 1
+	text "獸"
+	text "区", 9
+	text "猛", 0
+	line
+	text "請", 1
+	text "小", 0
+	text "心", 2
+	text "!", 1
+	done
