d9bdc09d98aab2ac3f6fec06ad1155c9102c751f
Zumi Daxuya <daxuya.zumi+code@protonmail.com>
2026-06-18 08:52:42 +0700

    bank63: expand `dr` to pointer list

--- banks/bank_63.asm
+++ banks/bank_63.asm
@@ -1,5 +1,120 @@
 Group05_Maps::
-	dr $18c000, $18c050
+	dw MapHeader_063_4050
+	dw MapHeader_063_4050
+	dw MapHeader_063_406e
+	dw MapHeader_063_4098
+	dw MapHeader_063_40ce
+	dw MapHeader_063_411c
+	dw MapHeader_063_416a
+	dw MapHeader_063_41f4
+	dw MapHeader_063_42ae
+	dw MapHeader_063_42cc
+	dw MapHeader_063_42f6
+	dw MapHeader_063_4314
+	dw MapHeader_063_4332
+	dw MapHeader_063_4350
+	dw MapHeader_063_4386
+	dw MapHeader_063_43b0
+	dw MapHeader_063_43c2
+	dw MapHeader_063_43d4
+	dw MapHeader_063_43f2
+	dw MapHeader_063_4428
+	dw MapHeader_063_443a
+	dw MapHeader_063_444c
+	dw MapHeader_063_4476
+	dw MapHeader_063_4494
+	dw MapHeader_063_44b2
+	dw MapHeader_063_44c4
+	dw MapHeader_063_44e2
+	dw MapHeader_063_450c
+	dw MapHeader_063_4536
+	dw MapHeader_063_4554
+	dw MapHeader_063_458a
+	dw MapHeader_063_459c
+	dw MapHeader_063_45ba
+	dw MapHeader_063_45cc
+	dw MapHeader_063_45de
+	dw MapHeader_063_45f0
+	dw MapHeader_063_460e
+	dw MapHeader_063_4638
+	dw MapHeader_063_464a
+	dw MapHeader_063_465c
 
 MapHeader_063_4050::
-	dr $18c050, $18c9d0
+	dr $18c050, $18c06e
+MapHeader_063_406e::
+	dr $18c06e, $18c098
+MapHeader_063_4098::
+	dr $18c098, $18c0ce
+MapHeader_063_40ce::
+	dr $18c0ce, $18c11c
+MapHeader_063_411c::
+	dr $18c11c, $18c16a
+MapHeader_063_416a::
+	dr $18c16a, $18c1f4
+MapHeader_063_41f4::
+	dr $18c1f4, $18c2ae
+MapHeader_063_42ae::
+	dr $18c2ae, $18c2cc
+MapHeader_063_42cc::
+	dr $18c2cc, $18c2f6
+MapHeader_063_42f6::
+	dr $18c2f6, $18c314
+MapHeader_063_4314::
+	dr $18c314, $18c332
+MapHeader_063_4332::
+	dr $18c332, $18c350
+MapHeader_063_4350::
+	dr $18c350, $18c386
+MapHeader_063_4386::
+	dr $18c386, $18c3b0
+MapHeader_063_43b0::
+	dr $18c3b0, $18c3c2
+MapHeader_063_43c2::
+	dr $18c3c2, $18c3d4
+MapHeader_063_43d4::
+	dr $18c3d4, $18c3f2
+MapHeader_063_43f2::
+	dr $18c3f2, $18c428
+MapHeader_063_4428::
+	dr $18c428, $18c43a
+MapHeader_063_443a::
+	dr $18c43a, $18c44c
+MapHeader_063_444c::
+	dr $18c44c, $18c476
+MapHeader_063_4476::
+	dr $18c476, $18c494
+MapHeader_063_4494::
+	dr $18c494, $18c4b2
+MapHeader_063_44b2::
+	dr $18c4b2, $18c4c4
+MapHeader_063_44c4::
+	dr $18c4c4, $18c4e2
+MapHeader_063_44e2::
+	dr $18c4e2, $18c50c
+MapHeader_063_450c::
+	dr $18c50c, $18c536
+MapHeader_063_4536::
+	dr $18c536, $18c554
+MapHeader_063_4554::
+	dr $18c554, $18c58a
+MapHeader_063_458a::
+	dr $18c58a, $18c59c
+MapHeader_063_459c::
+	dr $18c59c, $18c5ba
+MapHeader_063_45ba::
+	dr $18c5ba, $18c5cc
+MapHeader_063_45cc::
+	dr $18c5cc, $18c5de
+MapHeader_063_45de::
+	dr $18c5de, $18c5f0
+MapHeader_063_45f0::
+	dr $18c5f0, $18c60e
+MapHeader_063_460e::
+	dr $18c60e, $18c638
+MapHeader_063_4638::
+	dr $18c638, $18c64a
+MapHeader_063_464a::
+	dr $18c64a, $18c65c
+MapHeader_063_465c::
+	dr $18c65c, $18c9d0
