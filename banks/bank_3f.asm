d389dbb989cfddaa342696763f441147d0d834c9
Zumi Daxuya <daxuya.zumi+code@protonmail.com>
2026-06-20 21:11:33 +0700

    bank3f: MapObjectOam->BattleAnimObjectOam

--- banks/bank_3f.asm
+++ banks/bank_3f.asm
@@ -10,7 +10,7 @@ Func_03f_4000:
 	and a
 	jp z, .asm_40c8
 	ld [wd0c1], a
-	ld de, MapObjectOam_Pointers
+	ld de, BattleAnimObjectOam_Pointers
 	ld a, [wBattleAnimID]
 	sub $63
 	ld l, a
@@ -128,204 +128,204 @@ Func_03f_4000:
 	cp $E0
 	ret nc
 	jp .asm_4003
-MapObjectOam_Pointers:
-	dw MapObjectOamFrames_3f_4130
-	dw MapObjectOamFrames_3f_4284
-	dw MapObjectOamFrames_3f_4284
-	dw MapObjectOamFrames_3f_4284
-	dw MapObjectOamFrames_3f_43b5
-	dw MapObjectOamFrames_3f_4438
-	dw MapObjectOamFrames_3f_453e
-	dw MapObjectOamFrames_3f_4661
-	dw MapObjectOamFrames_3f_47ee
-	dw MapObjectOamFrames_3f_4919
-	dw MapObjectOamFrames_3f_4a27
-	dw MapObjectOamFrames_3f_4a27
-	dw MapObjectOamFrames_3f_4a27
-	dw MapObjectOamFrames_3f_4961
-	dw MapObjectOamFrames_3f_4a27
-	dw MapObjectOamFrames_3f_4a5d
-	dw MapObjectOamFrames_3f_4ad0
-	dw MapObjectOamFrames_3f_4b44
-	dw MapObjectOamFrames_3f_4c3a
-	dw MapObjectOamFrames_3f_4d10
-	dw MapObjectOamFrames_3f_4e9d
-	dw MapObjectOamFrames_3f_506c
-	dw MapObjectOamFrames_3f_50b7
-	dw MapObjectOamFrames_3f_517f
-	dw MapObjectOamFrames_3f_517f
-	dw MapObjectOamFrames_3f_5329
-	dw MapObjectOamFrames_3f_538e
-	dw MapObjectOamFrames_3f_5442
-	dw MapObjectOamFrames_3f_55e3
-	dw MapObjectOamFrames_3f_5767
-	dw MapObjectOamFrames_3f_5896
-	dw MapObjectOamFrames_3f_58f5
-	dw MapObjectOamFrames_3f_5a7a
-	dw MapObjectOamFrames_3f_5b5c
-	dw MapObjectOamFrames_3f_5c33
-	dw MapObjectOamFrames_3f_5d84
-	dw MapObjectOamFrames_3f_5ec6
-	dw MapObjectOamFrames_3f_5ec6
-	dw MapObjectOamFrames_3f_5f75
-	dw MapObjectOamFrames_3f_601d
-	dw MapObjectOamFrames_3f_6092
-	dw MapObjectOamFrames_3f_63e5
-	dw MapObjectOamFrames_3f_6660
-	dw MapObjectOamFrames_3f_6660
-	dw MapObjectOamFrames_3f_66e3
-	dw MapObjectOamFrames_3f_679e
-MapObjectOamFrames_3f_4130:
+BattleAnimObjectOam_Pointers:
+	dw BattleAnimObjectOamFrames_3f_4130
+	dw BattleAnimObjectOamFrames_3f_4284
+	dw BattleAnimObjectOamFrames_3f_4284
+	dw BattleAnimObjectOamFrames_3f_4284
+	dw BattleAnimObjectOamFrames_3f_43b5
+	dw BattleAnimObjectOamFrames_3f_4438
+	dw BattleAnimObjectOamFrames_3f_453e
+	dw BattleAnimObjectOamFrames_3f_4661
+	dw BattleAnimObjectOamFrames_3f_47ee
+	dw BattleAnimObjectOamFrames_3f_4919
+	dw BattleAnimObjectOamFrames_3f_4a27
+	dw BattleAnimObjectOamFrames_3f_4a27
+	dw BattleAnimObjectOamFrames_3f_4a27
+	dw BattleAnimObjectOamFrames_3f_4961
+	dw BattleAnimObjectOamFrames_3f_4a27
+	dw BattleAnimObjectOamFrames_3f_4a5d
+	dw BattleAnimObjectOamFrames_3f_4ad0
+	dw BattleAnimObjectOamFrames_3f_4b44
+	dw BattleAnimObjectOamFrames_3f_4c3a
+	dw BattleAnimObjectOamFrames_3f_4d10
+	dw BattleAnimObjectOamFrames_3f_4e9d
+	dw BattleAnimObjectOamFrames_3f_506c
+	dw BattleAnimObjectOamFrames_3f_50b7
+	dw BattleAnimObjectOamFrames_3f_517f
+	dw BattleAnimObjectOamFrames_3f_517f
+	dw BattleAnimObjectOamFrames_3f_5329
+	dw BattleAnimObjectOamFrames_3f_538e
+	dw BattleAnimObjectOamFrames_3f_5442
+	dw BattleAnimObjectOamFrames_3f_55e3
+	dw BattleAnimObjectOamFrames_3f_5767
+	dw BattleAnimObjectOamFrames_3f_5896
+	dw BattleAnimObjectOamFrames_3f_58f5
+	dw BattleAnimObjectOamFrames_3f_5a7a
+	dw BattleAnimObjectOamFrames_3f_5b5c
+	dw BattleAnimObjectOamFrames_3f_5c33
+	dw BattleAnimObjectOamFrames_3f_5d84
+	dw BattleAnimObjectOamFrames_3f_5ec6
+	dw BattleAnimObjectOamFrames_3f_5ec6
+	dw BattleAnimObjectOamFrames_3f_5f75
+	dw BattleAnimObjectOamFrames_3f_601d
+	dw BattleAnimObjectOamFrames_3f_6092
+	dw BattleAnimObjectOamFrames_3f_63e5
+	dw BattleAnimObjectOamFrames_3f_6660
+	dw BattleAnimObjectOamFrames_3f_6660
+	dw BattleAnimObjectOamFrames_3f_66e3
+	dw BattleAnimObjectOamFrames_3f_679e
+BattleAnimObjectOamFrames_3f_4130:
 	dr $fc130, $fc13e
-MapObjectOam_3f_413e:
+BattleAnimObjectOam_3f_413e:
 	dr $fc13e, $fc284
-MapObjectOamFrames_3f_4284:
+BattleAnimObjectOamFrames_3f_4284:
 	dr $fc284, $fc292
-MapObjectOam_3f_4292:
+BattleAnimObjectOam_3f_4292:
 	dr $fc292, $fc3b5
-MapObjectOamFrames_3f_43b5:
+BattleAnimObjectOamFrames_3f_43b5:
 	dr $fc3b5, $fc3bd
-MapObjectOam_3f_43bd:
+BattleAnimObjectOam_3f_43bd:
 	dr $fc3bd, $fc438
-MapObjectOamFrames_3f_4438:
+BattleAnimObjectOamFrames_3f_4438:
 	dr $fc438, $fc444
-MapObjectOam_3f_4444:
+BattleAnimObjectOam_3f_4444:
 	dr $fc444, $fc53e
-MapObjectOamFrames_3f_453e:
+BattleAnimObjectOamFrames_3f_453e:
 	dr $fc53e, $fc54a
-MapObjectOam_3f_454a:
+BattleAnimObjectOam_3f_454a:
 	dr $fc54a, $fc661
-MapObjectOamFrames_3f_4661:
+BattleAnimObjectOamFrames_3f_4661:
 	dr $fc661, $fc66d
-MapObjectOam_3f_466d:
+BattleAnimObjectOam_3f_466d:
 	dr $fc66d, $fc7ee
-MapObjectOamFrames_3f_47ee:
+BattleAnimObjectOamFrames_3f_47ee:
 	dr $fc7ee, $fc7f8
-MapObjectOam_3f_47f8:
+BattleAnimObjectOam_3f_47f8:
 	dr $fc7f8, $fc919
-MapObjectOamFrames_3f_4919:
+BattleAnimObjectOamFrames_3f_4919:
 	dr $fc919, $fc923
-MapObjectOam_3f_4923:
+BattleAnimObjectOam_3f_4923:
 	dr $fc923, $fc961
-MapObjectOamFrames_3f_4961:
+BattleAnimObjectOamFrames_3f_4961:
 	dr $fc961, $fc971
-MapObjectOam_3f_4971:
+BattleAnimObjectOam_3f_4971:
 	dr $fc971, $fca27
-MapObjectOamFrames_3f_4a27:
+BattleAnimObjectOamFrames_3f_4a27:
 	dr $fca27, $fca31
-MapObjectOam_3f_4a31:
+BattleAnimObjectOam_3f_4a31:
 	dr $fca31, $fca5d
-MapObjectOamFrames_3f_4a5d:
+BattleAnimObjectOamFrames_3f_4a5d:
 	dr $fca5d, $fca69
-MapObjectOam_3f_4a69:
+BattleAnimObjectOam_3f_4a69:
 	dr $fca69, $fcad0
-MapObjectOamFrames_3f_4ad0:
+BattleAnimObjectOamFrames_3f_4ad0:
 	dr $fcad0, $fcad8
-MapObjectOam_3f_4ad8:
+BattleAnimObjectOam_3f_4ad8:
 	dr $fcad8, $fcb44
-MapObjectOamFrames_3f_4b44:
+BattleAnimObjectOamFrames_3f_4b44:
 	dr $fcb44, $fcb4c
-MapObjectOam_3f_4b4c:
+BattleAnimObjectOam_3f_4b4c:
 	dr $fcb4c, $fcc3a
-MapObjectOamFrames_3f_4c3a:
+BattleAnimObjectOamFrames_3f_4c3a:
 	dr $fcc3a, $fcc44
-MapObjectOam_3f_4c44:
+BattleAnimObjectOam_3f_4c44:
 	dr $fcc44, $fcd10
-MapObjectOamFrames_3f_4d10:
+BattleAnimObjectOamFrames_3f_4d10:
 	dr $fcd10, $fcd26
-MapObjectOam_3f_4d26:
+BattleAnimObjectOam_3f_4d26:
 	dr $fcd26, $fce9d
-MapObjectOamFrames_3f_4e9d:
+BattleAnimObjectOamFrames_3f_4e9d:
 	dr $fce9d, $fcead
-MapObjectOam_3f_4ead:
+BattleAnimObjectOam_3f_4ead:
 	dr $fcead, $fd06c
-MapObjectOamFrames_3f_506c:
+BattleAnimObjectOamFrames_3f_506c:
 	dr $fd06c, $fd078
-MapObjectOam_3f_5078:
+BattleAnimObjectOam_3f_5078:
 	dr $fd078, $fd0b7
-MapObjectOamFrames_3f_50b7:
+BattleAnimObjectOamFrames_3f_50b7:
 	dr $fd0b7, $fd0c5
-MapObjectOam_3f_50c5:
+BattleAnimObjectOam_3f_50c5:
 	dr $fd0c5, $fd17f
-MapObjectOamFrames_3f_517f:
+BattleAnimObjectOamFrames_3f_517f:
 	dr $fd17f, $fd191
-MapObjectOam_3f_5191:
+BattleAnimObjectOam_3f_5191:
 	dr $fd191, $fd329
-MapObjectOamFrames_3f_5329:
+BattleAnimObjectOamFrames_3f_5329:
 	dr $fd329, $fd331
-MapObjectOam_3f_5331:
+BattleAnimObjectOam_3f_5331:
 	dr $fd331, $fd38e
-MapObjectOamFrames_3f_538e:
+BattleAnimObjectOamFrames_3f_538e:
 	dr $fd38e, $fd39c
-MapObjectOam_3f_539c:
+BattleAnimObjectOam_3f_539c:
 	dr $fd39c, $fd442
-MapObjectOamFrames_3f_5442:
+BattleAnimObjectOamFrames_3f_5442:
 	dr $fd442, $fd458
-MapObjectOam_3f_5458:
+BattleAnimObjectOam_3f_5458:
 	dr $fd458, $fd5e3
-MapObjectOamFrames_3f_55e3:
+BattleAnimObjectOamFrames_3f_55e3:
 	dr $fd5e3, $fd5f3
-MapObjectOam_3f_55f3:
+BattleAnimObjectOam_3f_55f3:
 	dr $fd5f3, $fd767
-MapObjectOamFrames_3f_5767:
+BattleAnimObjectOamFrames_3f_5767:
 	dr $fd767, $fd777
-MapObjectOam_3f_5777:
+BattleAnimObjectOam_3f_5777:
 	dr $fd777, $fd896
-MapObjectOamFrames_3f_5896:
+BattleAnimObjectOamFrames_3f_5896:
 	dr $fd896, $fd8a4
-MapObjectOam_3f_58a4:
+BattleAnimObjectOam_3f_58a4:
 	dr $fd8a4, $fd8f5
-MapObjectOamFrames_3f_58f5:
+BattleAnimObjectOamFrames_3f_58f5:
 	dr $fd8f5, $fd909
-MapObjectOam_3f_5909:
+BattleAnimObjectOam_3f_5909:
 	dr $fd909, $fda7a
-MapObjectOamFrames_3f_5a7a:
+BattleAnimObjectOamFrames_3f_5a7a:
 	dr $fda7a, $fda86
-MapObjectOam_3f_5a86:
+BattleAnimObjectOam_3f_5a86:
 	dr $fda86, $fdb5c
-MapObjectOamFrames_3f_5b5c:
+BattleAnimObjectOamFrames_3f_5b5c:
 	dr $fdb5c, $fdb6a
-MapObjectOam_3f_5b6a:
+BattleAnimObjectOam_3f_5b6a:
 	dr $fdb6a, $fdc33
-MapObjectOamFrames_3f_5c33:
+BattleAnimObjectOamFrames_3f_5c33:
 	dr $fdc33, $fdc3f
-MapObjectOam_3f_5c3f:
+BattleAnimObjectOam_3f_5c3f:
 	dr $fdc3f, $fdd84
-MapObjectOamFrames_3f_5d84:
+BattleAnimObjectOamFrames_3f_5d84:
 	dr $fdd84, $fdd90
-MapObjectOam_3f_5d90:
+BattleAnimObjectOam_3f_5d90:
 	dr $fdd90, $fdec6
-MapObjectOamFrames_3f_5ec6:
+BattleAnimObjectOamFrames_3f_5ec6:
 	dr $fdec6, $fded4
-MapObjectOam_3f_5ed4:
+BattleAnimObjectOam_3f_5ed4:
 	dr $fded4, $fdf75
-MapObjectOamFrames_3f_5f75:
+BattleAnimObjectOamFrames_3f_5f75:
 	dr $fdf75, $fdf7b
-MapObjectOam_3f_5f7b:
+BattleAnimObjectOam_3f_5f7b:
 	dr $fdf7b, $fe01d
-MapObjectOamFrames_3f_601d:
+BattleAnimObjectOamFrames_3f_601d:
 	dr $fe01d, $fe029
-MapObjectOam_3f_6029:
+BattleAnimObjectOam_3f_6029:
 	dr $fe029, $fe092
-MapObjectOamFrames_3f_6092:
+BattleAnimObjectOamFrames_3f_6092:
 	dr $fe092, $fe0b8
-MapObjectOam_3f_60b8:
+BattleAnimObjectOam_3f_60b8:
 	dr $fe0b8, $fe3e5
-MapObjectOamFrames_3f_63e5:
+BattleAnimObjectOamFrames_3f_63e5:
 	dr $fe3e5, $fe3f3
-MapObjectOam_3f_63f3:
+BattleAnimObjectOam_3f_63f3:
 	dr $fe3f3, $fe660
-MapObjectOamFrames_3f_6660:
+BattleAnimObjectOamFrames_3f_6660:
 	dr $fe660, $fe668
-MapObjectOam_3f_6668:
+BattleAnimObjectOam_3f_6668:
 	dr $fe668, $fe6e3
-MapObjectOamFrames_3f_66e3:
+BattleAnimObjectOamFrames_3f_66e3:
 	dr $fe6e3, $fe6ef
-MapObjectOam_3f_66ef:
+BattleAnimObjectOam_3f_66ef:
 	dr $fe6ef, $fe79e
-MapObjectOamFrames_3f_679e:
+BattleAnimObjectOamFrames_3f_679e:
 	dr $fe79e, $fe7a8
-MapObjectOam_3f_67a8:
+BattleAnimObjectOam_3f_67a8:
 	dr $fe7a8, $fe850
 
 
