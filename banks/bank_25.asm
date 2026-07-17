a2701b6f160b68f9374bf93bb9c91154cf7e18cc
Zumi Daxuya <daxuya.zumi+code@protonmail.com>
2026-07-17 17:16:27 +0700

    split up dex attr/tilemaps

--- banks/bank_25.asm
+++ banks/bank_25.asm
@@ -4144,12 +4144,24 @@ Palette_BattleUIObj::
 
 INCLUDE "text/menu_25_6199.asm"
 
-Tilemap_MonInfoBox7x6:: INCBIN "gfx/tilemaps/tilemap_025_61c4.tilemap"
-AttrMap_MonInfoBox7x6:: INCBIN "gfx/attrmaps/attrmap_025_61f2.bin"
-Tilemap_MonInfoBox8x8:: INCBIN "gfx/tilemaps/tilemap_025_6220.tilemap"
-AttrMap_MonInfoBox8x8:: INCBIN "gfx/attrmaps/attrmap_025_6264.bin"
-Tilemap_MonInfoBox8x10:: INCBIN "gfx/tilemaps/tilemap_025_62a8.tilemap"
-AttrMap_MonInfoBox8x10:: INCBIN "gfx/attrmaps/attrmap_025_62fc.bin"
+Tilemap_MonInfoBox7x6:: db $07, $06
+	dw $998d
+	INCBIN "gfx/tilemaps/tilemap_025_61c4.tilemap"
+AttrMap_MonInfoBox7x6:: db $07, $06
+	dw $998d
+	INCBIN "gfx/attrmaps/attrmap_025_61f2.bin"
+Tilemap_MonInfoBox8x8:: db $08, $08
+	dw $9940
+	INCBIN "gfx/tilemaps/tilemap_025_6220.tilemap"
+AttrMap_MonInfoBox8x8:: db $08, $08
+	dw $9940
+	INCBIN "gfx/attrmaps/attrmap_025_6264.bin"
+Tilemap_MonInfoBox8x10:: db $08, $0a
+	dw $9900
+	INCBIN "gfx/tilemaps/tilemap_025_62a8.tilemap"
+AttrMap_MonInfoBox8x10:: db $08, $0a
+	dw $9900
+	INCBIN "gfx/attrmaps/attrmap_025_62fc.bin"
 Tilemap_OverwriteConfirm20x6:: db $14, $06
 	INCBIN "gfx/tilemaps/tilemap_025_6350.tilemap"
 Tilemap_SaveResult20x6:: db $14, $06
