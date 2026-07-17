48d0f3f465a05402e79caa6e17b74281666b73cb
Zumi Daxuya <daxuya.zumi+code@protonmail.com>
2026-07-17 19:28:23 +0700

    bank15: missed a bit

---
+++ data/faces/pics2.asm
@@ -0,0 +1,125 @@
+; Second face table GFX (wTextFaceID >= PIC_GFX_2). Mirrors data/faces/pics.asm.
+FacePicPointers2:
+	dw face_58 ; $3a
+	dw face_59 ; $3b
+	dw face_60 ; $3c
+	dw face_61 ; $3d
+	dw face_62 ; $3e
+	dw face_63 ; $3f
+	dw face_64 ; $40
+	dw face_65 ; $41
+	dw face_66 ; $42
+	dw face_67 ; $43
+	dw face_68 ; $44
+	dw face_69 ; $45
+	dw face_70 ; $46
+	dw face_71 ; $47
+	dw face_72 ; $48
+	dw face_73 ; $49
+	dw face_74 ; $4a
+	dw face_75 ; $4b
+	dw face_76 ; $4c
+	dw face_77 ; $4d
+	dw face_78 ; $4e
+	dw face_79 ; $4f
+	dw face_80 ; $50
+	dw face_81 ; $51
+	dw face_82 ; $52
+	dw face_83 ; $53
+	dw face_84 ; $54
+	dw face_84 ; $55
+	dw face_86 ; $56
+	dw face_87 ; $57
+	dw face_88 ; $58
+	dw face_89 ; $59
+	dw face_89 ; $5a
+	dw face_91 ; $5b
+	dw face_91 ; $5c
+	dw face_93 ; $5d
+	dw face_94 ; $5e
+	dw face_95 ; $5f
+	dw face_96 ; $60
+	dw face_97 ; $61
+	dw face_98 ; $62
+	dw face_99 ; $63
+	dw face_100 ; $64
+	dw face_101 ; $65
+	dw face_102 ; $66
+	dw face_103 ; $67
+	dw face_104 ; $68
+	dw face_104 ; $69
+	dw face_106 ; $6a
+	dw face_107 ; $6b
+	dw face_108 ; $6c
+	dw face_109 ; $6d
+	dw face_110 ; $6e
+	dw face_111 ; $6f
+	dw face_112 ; $70
+	dw face_113 ; $71
+	dw face_113 ; $72
+	dw face_115 ; $73
+	dw face_116 ; $74
+	dw face_117 ; $75
+	dw face_118 ; $76
+	dw face_119 ; $77
+	dw face_120 ; $78
+
+face_58:: INCBIN "gfx/faces/face_58.bg.2bpp"
+face_59:: INCBIN "gfx/faces/face_59.bg.2bpp"
+face_60:: INCBIN "gfx/faces/face_60.bg.2bpp"
+face_61:: INCBIN "gfx/faces/face_61.bg.2bpp"
+face_62:: INCBIN "gfx/faces/face_62.bg.2bpp"
+face_63:: INCBIN "gfx/faces/face_63.bg.2bpp"
+face_64:: INCBIN "gfx/faces/face_64.bg.2bpp"
+face_65:: INCBIN "gfx/faces/face_65.bg.2bpp"
+face_66:: INCBIN "gfx/faces/face_66.bg.2bpp"
+face_67:: INCBIN "gfx/faces/face_67.bg.2bpp"
+face_68:: INCBIN "gfx/faces/face_68.bg.2bpp"
+face_69:: INCBIN "gfx/faces/face_69.bg.2bpp"
+face_70:: INCBIN "gfx/faces/face_70.bg.2bpp"
+face_71:: INCBIN "gfx/faces/face_71.bg.2bpp"
+face_72:: INCBIN "gfx/faces/face_72.bg.2bpp"
+face_73:: INCBIN "gfx/faces/face_73.bg.2bpp"
+face_74:: INCBIN "gfx/faces/face_74.bg.2bpp"
+face_75:: INCBIN "gfx/faces/face_75.bg.2bpp"
+face_76:: INCBIN "gfx/faces/face_76.bg.2bpp"
+face_77:: INCBIN "gfx/faces/face_77.bg.2bpp"
+face_78:: INCBIN "gfx/faces/face_78.bg.2bpp"
+face_79:: INCBIN "gfx/faces/face_79.bg.2bpp"
+face_80:: INCBIN "gfx/faces/face_80.bg.2bpp"
+face_81:: INCBIN "gfx/faces/face_81.bg.2bpp"
+face_82:: INCBIN "gfx/faces/face_82.bg.2bpp"
+face_83:: INCBIN "gfx/faces/face_83.bg.2bpp"
+face_84:: INCBIN "gfx/faces/face_84.bg.2bpp"
+face_86:: INCBIN "gfx/faces/face_86.bg.2bpp"
+face_87:: INCBIN "gfx/faces/face_87.bg.2bpp"
+face_88:: INCBIN "gfx/faces/face_88.bg.2bpp"
+face_89:: INCBIN "gfx/faces/face_89.bg.2bpp"
+face_91:: INCBIN "gfx/faces/face_91.bg.2bpp"
+face_93:: INCBIN "gfx/faces/face_93.bg.2bpp"
+face_94:: INCBIN "gfx/faces/face_94.bg.2bpp"
+face_95:: INCBIN "gfx/faces/face_95.bg.2bpp"
+face_96:: INCBIN "gfx/faces/face_96.bg.2bpp"
+face_97:: INCBIN "gfx/faces/face_97.bg.2bpp"
+face_98:: INCBIN "gfx/faces/face_98.bg.2bpp"
+face_99:: INCBIN "gfx/faces/face_99.bg.2bpp"
+face_100:: INCBIN "gfx/faces/face_100.bg.2bpp"
+face_101:: INCBIN "gfx/faces/face_101.bg.2bpp"
+face_102:: INCBIN "gfx/faces/face_102.bg.2bpp"
+face_103:: INCBIN "gfx/faces/face_103.bg.2bpp"
+face_104:: INCBIN "gfx/faces/face_104.bg.2bpp"
+face_106:: INCBIN "gfx/faces/face_106.bg.2bpp"
+face_107:: INCBIN "gfx/faces/face_107.bg.2bpp"
+face_108:: INCBIN "gfx/faces/face_108.bg.2bpp"
+face_109:: INCBIN "gfx/faces/face_109.bg.2bpp"
+face_110:: INCBIN "gfx/faces/face_110.bg.2bpp"
+face_111:: INCBIN "gfx/faces/face_111.bg.2bpp"
+face_112:: INCBIN "gfx/faces/face_112.bg.2bpp"
+face_113:: INCBIN "gfx/faces/face_113.bg.2bpp"
+face_115:: INCBIN "gfx/faces/face_115.bg.2bpp"
+face_116:: INCBIN "gfx/faces/face_116.bg.2bpp"
+face_117:: INCBIN "gfx/faces/face_117.bg.2bpp"
+face_118:: INCBIN "gfx/faces/face_118.bg.2bpp"
+face_119:: INCBIN "gfx/faces/face_119.bg.2bpp"
+face_120:: INCBIN "gfx/faces/face_120.bg.2bpp"
+	db $00, $00 ; tail padding
