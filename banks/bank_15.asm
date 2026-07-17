b17e966fe1d8a48d724b7778eea275a9f3f2ddb5
Zumi Daxuya <daxuya.zumi+code@protonmail.com>
2026-07-17 19:25:39 +0700

    bank15: missed this bit

--- banks/bank_15.asm
+++ banks/bank_15.asm
@@ -1,3 +1,33 @@
-_LoadTextFaceGFX2:: INCBIN "gfx/misc/_loadtextfacegfx2.2bpp"
+; Face-pic loader for wTextFaceID >= PIC_GFX_2 (second face table).
+; Twin of _LoadTextFaceGFX (banks/bank_1f.asm) for IDs < PIC_GFX_2.
+_LoadTextFaceGFX2::
+; 2bpp
+	ld de, FacePicPointers2
+	ld a, [wTextFaceID]
+	sub a, PIC_GFX_2
+	ld l, a
+	ld h, $00
+	add hl, hl
+	push hl
+	add hl, de
+	ld a, [hli]
+	ld h, [hl]
+	ld l, a
+	ld de, $8f00
+	ld bc, $100
+	call CopyBytesVRAM
 
+; palette
+	ld de, FacePalettePointers2
+	pop hl
+	add hl, de
+	ld a, [hli]
+	ld h, [hl]
+	ld l, a
+	ld b, 1 palettes
+	ld c, BCPSF_AUTOINC | $38
+	call LoadPalettes_BCPD
+	ret
 
+INCLUDE "data/faces/palettes2.asm"
+INCLUDE "data/faces/pics2.asm"
---
+++ data/faces/palettes2.asm
@@ -0,0 +1,113 @@
+; Second face table palettes. Mirrors data/faces/palettes.asm.
+FacePalettePointers2:
+	dw palette_015_58 ; $3a
+	dw palette_015_59 ; $3b
+	dw palette_015_60 ; $3c
+	dw palette_015_60 ; $3d
+	dw palette_015_60 ; $3e
+	dw palette_015_63 ; $3f
+	dw palette_015_63 ; $40
+	dw palette_015_63 ; $41
+	dw palette_015_63 ; $42
+	dw palette_015_67 ; $43
+	dw palette_015_67 ; $44
+	dw palette_015_67 ; $45
+	dw palette_015_67 ; $46
+	dw palette_015_71 ; $47
+	dw palette_015_71 ; $48
+	dw palette_015_71 ; $49
+	dw palette_015_71 ; $4a
+	dw palette_015_75 ; $4b
+	dw palette_015_76 ; $4c
+	dw palette_015_77 ; $4d
+	dw palette_015_78 ; $4e
+	dw palette_015_79 ; $4f
+	dw palette_015_80 ; $50
+	dw palette_015_81 ; $51
+	dw palette_015_82 ; $52
+	dw palette_015_83 ; $53
+	dw palette_015_84 ; $54
+	dw palette_015_84 ; $55
+	dw palette_015_86 ; $56
+	dw palette_015_87 ; $57
+	dw palette_015_88 ; $58
+	dw palette_015_89 ; $59
+	dw palette_015_89 ; $5a
+	dw palette_015_91 ; $5b
+	dw palette_015_91 ; $5c
+	dw palette_015_93 ; $5d
+	dw palette_015_94 ; $5e
+	dw palette_015_95 ; $5f
+	dw palette_015_96 ; $60
+	dw palette_015_97 ; $61
+	dw palette_015_98 ; $62
+	dw palette_015_99 ; $63
+	dw palette_015_100 ; $64
+	dw palette_015_101 ; $65
+	dw palette_015_102 ; $66
+	dw palette_015_103 ; $67
+	dw palette_015_104 ; $68
+	dw palette_015_104 ; $69
+	dw palette_015_106 ; $6a
+	dw palette_015_107 ; $6b
+	dw palette_015_108 ; $6c
+	dw palette_015_109 ; $6d
+	dw palette_015_110 ; $6e
+	dw palette_015_111 ; $6f
+	dw palette_015_112 ; $70
+	dw palette_015_113 ; $71
+	dw palette_015_113 ; $72
+	dw palette_015_115 ; $73
+	dw palette_015_116 ; $74
+	dw palette_015_117 ; $75
+	dw palette_015_118 ; $76
+	dw palette_015_119 ; $77
+	dw palette_015_120 ; $78
+
+palette_015_58: INCBIN "gfx/faces/face_58.bg.gbcpal"
+palette_015_59: INCBIN "gfx/faces/face_59.bg.gbcpal"
+palette_015_60: INCBIN "gfx/faces/face_60.bg.gbcpal"
+palette_015_63: INCBIN "gfx/faces/face_63.bg.gbcpal"
+palette_015_67: INCBIN "gfx/faces/face_67.bg.gbcpal"
+palette_015_71: INCBIN "gfx/faces/face_71.bg.gbcpal"
+palette_015_75: INCBIN "gfx/faces/face_75.bg.gbcpal"
+palette_015_76: INCBIN "gfx/faces/face_76.bg.gbcpal"
+palette_015_77: INCBIN "gfx/faces/face_77.bg.gbcpal"
+palette_015_78: INCBIN "gfx/faces/face_78.bg.gbcpal"
+palette_015_79: INCBIN "gfx/faces/face_79.bg.gbcpal"
+palette_015_80: INCBIN "gfx/faces/face_80.bg.gbcpal"
+palette_015_81: INCBIN "gfx/faces/face_81.bg.gbcpal"
+palette_015_82: INCBIN "gfx/faces/face_82.bg.gbcpal"
+palette_015_83: INCBIN "gfx/faces/face_83.bg.gbcpal"
+palette_015_84: INCBIN "gfx/faces/face_84.bg.gbcpal"
+palette_015_86: INCBIN "gfx/faces/face_86.bg.gbcpal"
+palette_015_87: INCBIN "gfx/faces/face_87.bg.gbcpal"
+palette_015_88: INCBIN "gfx/faces/face_88.bg.gbcpal"
+palette_015_89: INCBIN "gfx/faces/face_89.bg.gbcpal"
+palette_015_91: INCBIN "gfx/faces/face_91.bg.gbcpal"
+palette_015_93: INCBIN "gfx/faces/face_93.bg.gbcpal"
+palette_015_94: INCBIN "gfx/faces/face_94.bg.gbcpal"
+palette_015_95: INCBIN "gfx/faces/face_95.bg.gbcpal"
+palette_015_96: INCBIN "gfx/faces/face_96.bg.gbcpal"
+palette_015_97: INCBIN "gfx/faces/face_97.bg.gbcpal"
+palette_015_98: INCBIN "gfx/faces/face_98.bg.gbcpal"
+palette_015_99: INCBIN "gfx/faces/face_99.bg.gbcpal"
+palette_015_100: INCBIN "gfx/faces/face_100.bg.gbcpal"
+palette_015_101: INCBIN "gfx/faces/face_101.bg.gbcpal"
+palette_015_102: INCBIN "gfx/faces/face_102.bg.gbcpal"
+palette_015_103: INCBIN "gfx/faces/face_103.bg.gbcpal"
+palette_015_104: INCBIN "gfx/faces/face_104.bg.gbcpal"
+palette_015_106: INCBIN "gfx/faces/face_106.bg.gbcpal"
+palette_015_107: INCBIN "gfx/faces/face_107.bg.gbcpal"
+palette_015_108: INCBIN "gfx/faces/face_108.bg.gbcpal"
+palette_015_109: INCBIN "gfx/faces/face_109.bg.gbcpal"
+palette_015_110: INCBIN "gfx/faces/face_110.bg.gbcpal"
+palette_015_111: INCBIN "gfx/faces/face_111.bg.gbcpal"
+palette_015_112: INCBIN "gfx/faces/face_112.bg.gbcpal"
+palette_015_113: INCBIN "gfx/faces/face_113.bg.gbcpal"
+palette_015_115: INCBIN "gfx/faces/face_115.bg.gbcpal"
+palette_015_116: INCBIN "gfx/faces/face_116.bg.gbcpal"
+palette_015_117: INCBIN "gfx/faces/face_117.bg.gbcpal"
+palette_015_118: INCBIN "gfx/faces/face_118.bg.gbcpal"
+palette_015_119: INCBIN "gfx/faces/face_119.bg.gbcpal"
+palette_015_120: INCBIN "gfx/faces/face_120.bg.gbcpal"
